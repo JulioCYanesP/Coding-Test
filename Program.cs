a9a3a6f8badd15f5a2e3b1053df8424210543bbd
Julio Yanes <jyanes@shokworks.io>
2020-09-29 18:07:57 -0400

    Add movement handler

--- Program.cs
+++ Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks.Dataflow;
+using System.Transactions;
 
 namespace Coding_Test
 {
@@ -13,7 +16,8 @@ namespace Coding_Test
             int xInitialCoordinate;
             int yInitialCoordinate;
             string commandLine;
-            char orientation;
+            int int_orientation;
+            (int,int) currentPosition;
 
             //User inputs
 
@@ -35,16 +39,105 @@ namespace Coding_Test
             inputs = inputLine.Split(' ');
             xInitialCoordinate = Convert.ToInt32(inputs[0]);
             yInitialCoordinate = Convert.ToInt32(inputs[1]);
-            orientation = inputs[2][0];
+            int_orientation = getOrientationToInt(inputs[2][0]);
 
+            Console.WriteLine(int_orientation);
 
             Console.WriteLine("Enter command line");
             Console.WriteLine("Examples");
             Console.WriteLine("AALAARALA \n");
             commandLine = Console.ReadLine();
 
+            //Movement calculation
+
+            currentPosition = (xInitialCoordinate, yInitialCoordinate);
+            foreach (char command in commandLine)
+            {
+                switch (command)
+                {
+                    case 'A':
+                        currentPosition = advance(int_orientation, currentPosition);
+                        break;
+                    case 'L':
+                        int_orientation -= 1;
+                        if (int_orientation == 0)
+                            int_orientation = 4;
+                        break;
+                    case 'R':
+                        int_orientation += 1;
+                        if (int_orientation == 5)
+                            int_orientation = 1;
+                        break;
+                    default:
+                        //Will never enter here
+                        break;
+                }
+                    
+
+            }
+            
 
         }
 
+        private static int getOrientationToInt(char orientation)
+        {
+            switch (orientation)
+            {
+                case 'N':
+                    return 1;
+                case 'E':
+                    return 2;
+                case 'S':
+                    return 3;
+                case 'W':
+                    return 4;
+                default:
+                    return -1;
+            }
+        }
+
+        private static int getOrientationToChar(int orientation)
+        {
+            switch (orientation)
+            {
+                case 1:
+                    return 'N';
+                case 2:
+                    return 'E';
+                case 3:
+                    return 'S';
+                case 4:
+                    return 'w';
+                default:
+                    return 'X';
+            }
+        }
+
+        private static (int,int) advance(int orientation, (int,int) currentPosition)
+        {
+            switch(orientation)
+            {
+                case 1:       // North                    
+                    return (currentPosition.Item1,currentPosition.Item2 + 1);
+                case 2:       // East
+                    return (currentPosition.Item1 + 1 , currentPosition.Item2);
+                case 3:       // South
+                    return (currentPosition.Item1, currentPosition.Item2 - 1);
+                case 4:       // West
+                    return (currentPosition.Item1 - 1, currentPosition.Item2);
+                default:
+                    return (-1,-1);
+            }
+        }
+
+        private static bool checkPosition (int w, int h, (int,int) currentPostion)
+        {
+            if (currentPostion.Item1 >= w || currentPostion.Item1 < 0)
+                return false;
+            if (currentPostion.Item2 >= h || currentPostion.Item2 < 0)
+                return false;
+
+            return true;
+        }
     }
 }
